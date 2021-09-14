c01d911d2074f2d423b417bbc5e8d51c4f2c4461
Challalikitha <85385250+Challalikitha@users.noreply.github.com>
2021-09-14 18:46:21 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    const toggleMenu=document.querySelector('.toggleMenu');
-    const navigation=document.querySelector('.navigation');
-    toggleMenu.classList.toggle('active')
-    navigation.classList.toggle('active')
-}
